7d3bad24da47450f36b665fa5ed825683e38570b
darwin1501 <darwin.pablo1501@gmail.com>
2020-07-01 15:19:14 +0800

    add some changes

--- js/logic.js
+++ js/logic.js
@@ -3,7 +3,6 @@ const covid19Data1 = 'https://disease.sh/v3/covid-19/countries?yesterday=false&a
 
 const covid19Data2 = 'https://disease.sh/v3/covid-19/countries?yesterday=true&allowNull=false';
 
-
 async function getData(){
 
     let todaysData = await fetch(covid19Data1);
@@ -20,19 +19,21 @@ async function getData(){
    
    //clear session storage
     sessionStorage.clear()
-
     //store in session
     sessionStorage.newData = JSON.stringify(newData);
+
     sessionStorage.oldData = JSON.stringify(oldData);
 
 	generateOption(newData);
 
-   
   }else{
+
     console.log("HTTP-Error: " + response.status);
+
   }
 
   return responseArray;
+
 }
 
 getData();
@@ -41,23 +42,11 @@ getData();
 const distributeData = (() => {
 
 	const newDataParsed = JSON.parse( sessionStorage.newData )
+
 	const oldDataParsed = JSON.parse( sessionStorage.oldData )
 
 	getDetailedCase(newDataParsed, oldDataParsed)
 })
-
-
-
-
-// //session store
-// // Store
-// sessionStorage.setItem("lastname", "Smith");
-// // Retrieve
-// document.getElementById("result").innerHTML = sessionStorage.getItem("lastname");
-
-
-
-
 
 const generateOption = ((data) => {
 
@@ -85,9 +74,9 @@ const getDetailedCase = ((newData, oldData) => {
 
 		const selectedCountry = document.getElementById('selection').value
 
-		const todaysData = newData.find(xyz => xyz.country === selectedCountry)
+		const todaysData = newData.find(data => data.country === selectedCountry)
 
-		const yesterdaysData = oldData.find(xyz => xyz.country === selectedCountry)
+		const yesterdaysData = oldData.find(data => data.country === selectedCountry)
 		//total case today
 		// console.log(`Today Cases at ${todaysData.country} : ${todaysData.todayCases}`)
 		// //total recovery today
@@ -98,31 +87,14 @@ const getDetailedCase = ((newData, oldData) => {
 		console.log(`Todays total case at ${todaysData.country}: ${todaysData.cases}`)
 		//yesterday
 		console.log(`Yesterdays total case at ${yesterdaysData.country}: ${yesterdaysData.cases}`)
+
+		//get percentage
+		const addedCase = todaysData.cases - yesterdaysData.cases;
+
+		const divide = addedCase / yesterday
+		//calculate
+		console.log(`New Cases Added today at ${todaysData.country}: ${addedCase}`);
 		
 })
 
-// const rawData = ((data) => {
-// 		return data
-// })
 
-
-
-
-// //ES6
-// const findName = ((name) => {
-// 	let data = json.find(xyz => xyz.fullname === name)
-// 	return data
-// })
-// const result = findName('Jhon Doe');
-// //destructuring objects
-// const {fullname: name ,current_item: item} = result;
-
-// console.log(`${name} has the ${item}`)
-
-
-
-//display all recovery
-// const displayData = ((json) => {
-// 	const recovery = json;
-// 	return console.log(`Total Recovery ${recovery}`)
-// })
